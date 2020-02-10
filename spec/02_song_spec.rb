0369cb29703657ac861654be36ba1cdfc2083ba4
Pam Cairo <pcairo_90@outlook.com>
2020-02-10 18:19:20 +0000

    Done.

--- spec/02_song_spec.rb
+++ spec/02_song_spec.rb
@@ -1,44 +1,26 @@
 require "spec_helper"
 
 describe "Song" do
+  let!(:rap) { Genre.new("rap") }
+  let!(:ninety_nine_problems) { Song.new("99 problems", rap) } 
+  let!(:jay_z) { Artist.new("Jay-Z") }
 
-  describe "#new" do
-    it "initializes with a name, an artist, and a genre" do
-      rap = Genre.new("rap")
-      jay_z = Artist.new("Jay-Z")
-
-      expect{Song.new("Lucifer", jay_z, rap)}.to_not raise_error
-    end
-  end
-
-  describe ".all" do
-    it "knows about all song instances" do
-      jay_z = Artist.new("Jay-Z")
-      rap = Genre.new("rap")
-      Song.new("99 Problems", jay_z, rap)
-      shrimp = Song.new("Big Shrimpin", jay_z, rap)
-
-      expect(Song.all).to include(shrimp)
+  describe "#new" do 
+    it "initializes with a name and a genre" do 
+      expect{Song.new("Lucifer", rap)}.to_not raise_error
     end
   end
 
   describe "#genre" do
-    it "belongs to a genre" do
-      jay_z = Artist.new("Jay-Z")
-      rap = Genre.new("rap")
-      ninety_nine_problems = Song.new("99 Problems", jay_z, rap)
-
+    it "belongs to a genre" do 
       expect(ninety_nine_problems.genre).to eq(rap)
-    end
+    end 
   end
 
   describe "#artist" do
-    it "belongs to a artist" do
-      jay_z = Artist.new("Jay-Z")
-      rap = Genre.new("rap")
-      ninety_nine_problems = Song.new("99 Problems", jay_z, rap)
-
+    it "belongs to a artist" do 
+      ninety_nine_problems.artist = jay_z
       expect(ninety_nine_problems.artist).to eq(jay_z)
-    end
+    end 
   end
 end
--- spec/05_appointment_spec.rb
+++ spec/05_appointment_spec.rb
@@ -1,43 +1,27 @@
 require "spec_helper"
 
 describe "Appointment" do
+  let!(:martin) { Patient.new("Martin Jones") } 
+  let!(:doctor_who) { Doctor.new("The Doctor") }
+  let!(:appointment) { Appointment.new("Friday, January 32nd", doctor_who) }
 
-  describe ".all" do
-    it "knows about all appointments that have been created" do
-      doctor_who = Doctor.new("The Doctor")
-      hevydevy = Patient.new("Devin Townsend")
-      doctor_who.new_appointment(hevydevy, "Friday, January 32nd")
-      appointment = doctor_who.new_appointment(hevydevy, "Caturday, January Purrty2nd")
-      doctor_who.new_appointment(hevydevy, "Satunday, January 34nd")
-
-      expect(Appointment.all).to include(appointment)
-    end
-  end
-
-  describe "#new" do
-    it "initializes with a date, patient, and doctor" do
-      doctor_who = Doctor.new("The Doctor")
-      hevydevy = Patient.new("Devin Townsend")
-
-      expect{Appointment.new("Friday, January 32nd", hevydevy, doctor_who)}.to_not raise_error
+  describe "#new" do 
+    it "initializes with a date and a doctor" do 
+      expect{Appointment.new("Thursday, October 31st", doctor_who)}.to_not raise_error
     end
   end
 
   describe "#patient" do
-    it "belongs to a patient" do
-      doctor_who = Doctor.new("The Doctor")
-      hevydevy = Patient.new("Devin Townsend")
-      appointment = hevydevy.new_appointment(doctor_who, "Friday, January 32nd")
-      expect(appointment.patient).to eq(hevydevy)
-    end
+    it "belongs to a patient" do 
+      appointment.patient = martin
+      expect(appointment.patient).to eq(martin)
+    end 
   end
 
-  describe "#doctor" do
-    it "belongs to a doctor" do
-      doctor_who = Doctor.new("The Doctor")
-      hevydevy = Patient.new("Devin Townsend")
-      appointment = doctor_who.new_appointment(hevydevy, "Friday, January 32nd")
+  describe "#doctor" do 
+    it "belongs to a doctor" do 
       expect(appointment.doctor).to eq(doctor_who)
+      expect(doctor_who.appointments).to include(appointment)
     end
   end
 end
